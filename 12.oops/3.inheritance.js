9690f0ca096f65102552780a95887e0aa9291535
Jatin <gargjatin1000@gmail.com>
2021-08-16 13:26:47 +0530

    update 3.0

--- 12.oops/3.inheritance.js
+++ 12.oops/3.inheritance.js
@@ -1,3 +1,24 @@
 function Person(name){
     this.name=name
 }
+
+Person.prototype.getname=function(){
+    return this.name
+}
+
+function Student(age){
+    this.age=age
+}
+
+
+//inherit from pareent
+
+Student.prototype=new Person("jt")
+
+Student.prototype.getage=function(){
+    return this.age
+}
+
+var stud=new Student(22)
+console.log(stud)
+console.log(stud.getname())
